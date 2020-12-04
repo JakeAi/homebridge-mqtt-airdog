247174b93a3ae3a975927a41a40f1b8a2d4016d4
Jake Ihasz <JakeIhasz@minnich-mfg.com>
2020-12-04 13:26:40 -0500

    test

---
+++ dist/settings.d.ts
@@ -0,0 +1,9 @@
+/**
+ * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
+ */
+export declare const PLATFORM_NAME = "Airdog";
+/**
+ * This must match the name of your plugin as defined the package.json
+ */
+export declare const PLUGIN_NAME = "homebridge-mqtt-airdog";
+//# sourceMappingURL=settings.d.ts.map
